93e8a40107dece868a06df624161361932895346
keith void <niangaotao@gmail.com>
2017-11-01 18:07:27 +0800

    week 5 assingment

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -66,7 +66,6 @@ yr=zeros(m,num_labels);
 for i = 1:m
     yr(i,y(i))=1;
 end
-
 for i = 1:m
     a1=X(i,:);
     a2=sigmoid(a1*Theta1');
@@ -79,7 +78,41 @@ for i = 1:m
         J=J+sum(-yi.*log(hx)-(1-yi).*log(1-hx));
     end
 end
-J=J/m
+J=J/m;
+
+theta1=Theta1(:,2:end).^2;
+theta2=Theta2(:,2:end).^2;
+theta1_all=theta1(:);
+theta2_all=theta2(:);
+temp=sum(theta1_all)+sum(theta2_all);
+J=J+(lambda*temp)/(2*m);
+
+Delta1=zeros(size(Theta1));
+Delta2=zeros(size(Theta2));
+Delta2=0;
+for t =1:m
+    a1=X(t,:);% 401x1
+    z2=a1*Theta1'; %1x401x401x25=1x25
+    a2=sigmoid(z2);
+    a2=[1 a2]; %1x26
+    z3=a2*Theta2'; %1x26x26x10=1x10
+    a3=sigmoid(z3);%1x10
+    hx=a3'; % 10x1
+    yt=yr(t,:); % 1x10
+    yt=yt'; %1x10
+    delta3=hx-yt; % 10x1
+    %delta_2=Theta2'*delta_3; %26x10x10x1=26x1
+    %note:theta2 bias units should be ignored,u can't find this in ex4.pdf,so
+    delta2=Theta2(:,2:end)'*delta3; % now we have 25x1 vector
+    delta2=delta2.*sigmoidGradient(z2'); %25x1
+    Delta1=Delta1+delta2*a1;%25x1x1x401=25x401
+    Delta2=Delta2+delta3*a2;
+end
+Theta1_grad(:,1)=Delta1(:,1)/m;
+Theta1_grad(:,2:end)=Delta1(:,2:end)/m+(lambda/m)*Theta1(:,2:end);
+Theta2_grad(:,1)=Delta2(:,1)/m;
+Theta2_grad(:,2:end)=Delta2(:,2:end)/m+(lambda/m)*Theta2(:,2:end);
+
 
 
 
